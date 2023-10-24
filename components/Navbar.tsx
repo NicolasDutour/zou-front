2416de8992ae59c451b30cfb5feda30ee78a02c9
nicolas dutour <dutourn99@gmail.com>
2023-10-24 16:44:00 +0200

    feat: fixed height navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,14 @@ import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <header className='fixed w-full px-8 py-5 shadow-lg bg-gray-900 z-20'>
+    <header className='fixed w-full h-[77px] px-8 py-5 shadow-lg bg-gray-900 z-20'>
       <nav className='flex items-center justify-between max-w-7xl mx-auto'>
         <Link href="/">
           <Image
             src="/leaf-icon.png"
             alt="leaf-icon"
-            width={50}
-            height={50}
+            width={40}
+            height={40}
             aspect-auto="true"
             className="rounded-lg"
             priority
