54a3f43d170889f4edd8e8525cc03429db6c9481
nicolas dutour <dutourn99@gmail.com>
2023-11-02 11:42:53 +0100

    feat: add dialog for plan in home page

---
+++ components/DialogPlan.tsx
@@ -0,0 +1,33 @@
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export default function DialogPlan({ title, price, description }: { title: string, price: string, description: string }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className="w-full">
+        <p className="flex flex-col justify-between p-2 mb-2 shadow-lg rounded-xl border-2 border-secondary text-gray-600">
+          <span className="text-secondary text-lg"><span className="font-bold text-2xl"> {title} </span> pour <span className="font-bold text-2xl"> {price} €</span> / mois</span> {description}.
+        </p>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle> {title} </AlertDialogTitle>
+          <AlertDialogDescription>
+            {description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Close</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
