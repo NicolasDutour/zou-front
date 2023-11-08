34242692d60298076051194638acb256783207c1
nicolas dutour <dutourn99@gmail.com>
2023-11-08 10:47:27 +0100

    feat: add loading page

---
+++ app/admin/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center backdrop-blur-md backdrop-filter bg-white">
+      <div className="text-gray-700 text-xl font-semibold mt-24">Loading profile...</div>
+    </div>
+  )
+}
