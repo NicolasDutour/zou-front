e6598dc79dd428895a50d756eb866f21c48a16bc
nicolas dutour <dutourn99@gmail.com>
2023-11-09 11:37:35 +0100

    style: ui + images

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 import { ProductType } from "@/lib/types";
 import { formatIngredients } from "@/lib/utils";
 
@@ -23,13 +34,46 @@ const Product = ({ product }: { product: ProductType }) => {
   //   }
   // }
 
+  const baseImage = () => {
+    if (base === "tomato") {
+      return (
+        <Image
+          src="/tomato.png"
+          alt="tomato"
+          width={30}
+          height={30}
+        />
+      )
+    } else {
+      return (
+        <Image
+          src="/cream.png"
+          alt="cream"
+          width={30}
+          height={30}
+        />
+      )
+    }
+  }
+
   return (
-    <div className="flex justify-between items-center rounded-lg p-2 bg-white hover:shadow-lg transition-shadow">
-      <p className="uppercase font-semibold">{product_name || null}</p>
-      <p className="">{base || null}</p>
-      <p className="">{formatIngredients(ingredients) || null}</p>
-      <p className="text-red-800 p-4 text-2xl font-semibold">{price.toFixed(2) || null} €</p>
+    <div className="md:flex items-center border border-gray-200 rounded-lg p-4 bg-white">
+      <div className="w-full md:w-1/4">{product_name || null}</div>
+      <div className="mb-2 md:mb-0">{baseImage()}</div>
+      <div className="md:pl-6 mb-4 md:mb-0">
+        {formatIngredients(ingredients) || null}
+      </div>
+      <div className="ml-auto">
+        {price.toFixed(2) || null} €
+      </div>
     </div>
+
+    // <div className="flex justify-between items-center rounded-lg p-2 bg-white hover:shadow-lg transition-shadow">
+    //   <p className="uppercase font-semibold">{product_name || null}</p>
+    //   <p className="">{base || null}</p>
+    //   <p className="">{formatIngredients(ingredients) || null}</p>
+    //   <p className="text-red-800 p-4 text-2xl font-semibold">{price.toFixed(2) || null} €</p>
+    // </div>
   )
 }
 
