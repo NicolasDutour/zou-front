ed420eaffd9a9b648713726cacce2a6fda9db60a
nicolas dutour <dutourn99@gmail.com>
2023-11-09 16:30:31 +0100

    feat: remove block

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -54,12 +54,6 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
 
   return (
     <div className="space-y-6 p-10 pb-16 md:block">
-      <div className="space-y-0.5">
-        <p className="text-muted-foreground">
-          Abonnement actuel: <span className="underline underline-offset-4 font-semibold text-secondary">{capitalize(user?.pricing_plan?.title)}</span>
-        </p>
-      </div>
-      <Separator className="my-6" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
           <SidebarNav items={sidebarNavItems} />
