43f045accd2a44acc0c637caa3b4cf591ce528af
nicolas dutour <dutourn99@gmail.com>
2023-11-09 16:58:17 +0100

    style: change banner UI

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,30 +1,12 @@
-"use client"
-
-import { BsArrowDownCircle } from "react-icons/bs";
-import { Button } from "./ui/button"
-
 export default function Banner() {
-
-  const scrollDown = () => {
-    window.scrollBy(0, 500);
-  };
-
   return (
-    <section className="h-[600px] md:h-[500px] w-full">
-      <div className="relative p-6 bg-cover bg-center h-full bg-hero-pattern flex justify-center items-center">
-        <div className="bg-black opacity-70 absolute inset-0"></div>
-        <div className="mb-10 z-10 text-white">
-          <div className="max-w-5xl mx-auto gap-6 grid grid-cols-1 place-items-center">
-            <h1 className="text-8xl text-secondary">Ted</h1>
-            <p className="text-4xl">Votre site web prêt à l'emploi</p>
-            <p className="text-2xl">Comment ça fonctionne ?</p>
-            <Button className="text-white text-xl rounded-2xl py-10 mr-4 hover:bg-green-900" onClick={scrollDown} variant="secondary">
-              <div className='icon-move-down flex justify-center text-white text-2xl cursor-pointer'>
-                <BsArrowDownCircle />
-              </div>
-            </Button>
-          </div>
-        </div>
+    <section className="h-[600px] md:h-[500px] w-full bg-slate-100">
+      <div className="max-w-5xl mx-auto p-6 h-full flex flex-col justify-center items-center text-gray-500">
+        <h1 className="text-8xl text-secondary mb-6">Ted</h1>
+        <p className="text-4xl mb-4">Votre page web prêt à l'emploi</p>
+        <p className="text-xl">Un design simple est fonctionnel</p>
+        <p className="text-xl">Vous n'avez qu'à saisir les informations sur votre établissement</p>
+        <p className="text-xl">Et hop votre site est en ligne !</p>
       </div>
     </section>
   )
