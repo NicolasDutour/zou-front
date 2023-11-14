13e823bd12bf80dd05f9117b9235b108c9ff8443
nicolas dutour <dutourn99@gmail.com>
2023-11-14 17:18:06 +0100

    feat get subscription from db

--- app/admin/subscriptions/page.tsx
+++ app/admin/subscriptions/page.tsx
@@ -1,29 +1,29 @@
 import { cookies } from 'next/headers'
 
 import { Separator } from "@/components/ui/separator";
+import { capitalize } from '@/lib/utils';
 
-// async function getData() {
-//   const cookieStore = cookies()
-//   const token = cookieStore.get('token')?.value
-
-//   if (token) {
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/users/me?populate[restaurants][populate]=*&populate[pricing_plan][populate]=*`,
-//       {
-//         method: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${token}`
-//         }
-//       })
-//     if (!res.ok) {
-//       console.log("error");
-//     }
-//     return res.json()
-//   }
-// }
+async function getData(token: string) {
+  if (token) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/users/me?populate[restaurants][populate]=*&populate[pricing_plan][populate]=*`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
+    if (!res.ok) {
+      console.log("error");
+    }
+    return res.json()
+  }
+}
 
 export default async function SettingsSubscriptionsPage() {
-  // const data = await getData()
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')?.value
+  const data = await getData(token || '')
 
   return (
     <div className="space-y-6">
@@ -34,11 +34,11 @@ export default async function SettingsSubscriptionsPage() {
         </p>
       </div>
       <Separator />
-      <h2 className=''>Abonnement actuel</h2>
-      <p>plan ...</p>
-      <p> Price €/mois </p>
-      <p>Facturé mensuellement</p>
-      <p>Annulation le ... à ... (si annulation demandée)</p>
+      <h2 className='text-2xl'>Abonnement actuel</h2>
+      <p className='text-lg text-secondary'> {capitalize(data?.pricing_plan.title)}</p>
+      <p> <span className="font-bold text-2xl text-secondary">{data?.pricing_plan.price}</span> € / mois </p>
+      <p className='text-sm'>Facturé mensuellement</p>
+      {/* <p>Annulation le ... à ... (si annulation demandée)</p> */}
     </div>
   )
 }
