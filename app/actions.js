10ab4afc19f9892305f6d67cbb46843cfc97f123
nicolas dutour <dutourn99@gmail.com>
2023-11-20 16:12:23 +0100

    feat: token expires after 24h

--- app/actions.js
+++ app/actions.js
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 
 export async function createCookie(name, data) {
   const oneDay = 24 * 60 * 60 * 1000
-  cookies().set(name, data, { expires: Date.now() - oneDay })
+  cookies().set(name, data, { expires: Date.now() + oneDay })
 }
 
 export async function removeCookie(name) {
