88c8f13058d2a9164084a8fea471df933ab9f0ce
nicolas dutour <dutourn99@gmail.com>
2023-11-21 10:12:53 +0100

    feat: get pricing plans

--- app/admin/subscriptions/page.tsx
+++ app/admin/subscriptions/page.tsx
@@ -1,7 +1,8 @@
 import { cookies } from 'next/headers'
 
 import { Separator } from "@/components/ui/separator";
-import { capitalize } from '@/lib/utils';
+import { capitalize, cn } from '@/lib/utils';
+import { PlanType } from '@/lib/types';
 
 async function getData(token: string) {
   if (token) {
@@ -20,10 +21,25 @@ async function getData(token: string) {
   }
 }
 
+async function getDataPlans() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/pricing-plans`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    console.error('Failed to fetch data')
+  }
+  return res.json()
+}
+
 export default async function SettingsSubscriptionsPage() {
   const cookieStore = cookies()
   const token = cookieStore.get('token')?.value
   const data = await getData(token || '')
+  const plans: { data: PlanType[] } = await getDataPlans()
 
   return (
     <div className="space-y-6">
@@ -34,11 +50,50 @@ export default async function SettingsSubscriptionsPage() {
         </p>
       </div>
       <Separator />
-      <h2 className='text-2xl'>Abonnement actuel</h2>
-      <p className='text-lg text-secondary'> {capitalize(data?.pricing_plan.title)}</p>
-      <p> <span className="font-bold text-2xl text-secondary">{data?.pricing_plan.price}</span> € / mois </p>
-      <p className='text-sm'>Facturé mensuellement</p>
-      {/* <p>Annulation le ... à ... (si annulation demandée)</p> */}
+      {
+        data?.pricing_plan ? (
+          <>
+            <h2 className='text-2xl'>Abonnement actuel</h2>
+            <div className={cn("flex items-center w-full md:w-1/2 justify-between border-2 rounded-md mb-2", data?.pricing_plan.title === "essentiel" ? "border-secondary" : "border-primary")}>
+              <div className='p-4'>
+                <p className={cn('uppercase font-bold', data?.pricing_plan.title === "essentiel" ? "text-secondary" : "text-primary")}> {data?.pricing_plan.title} </p>
+                <p> {data?.pricing_plan.description} </p>
+                <p className='text-sm mt-6'>Facturé mensuellement</p>
+              </div>
+              <p className={cn('text-2xl text-white p-4 self-stretch grid place-items-center', data?.pricing_plan.title === "essentiel" ? "bg-secondary" : " bg-primary")}>{data?.pricing_plan.price} € / mois</p>
+            </div>
+            <div>
+              <p>Date début abonnement: ...</p>
+              <p>Date fin abonnement: ...</p>
+              <p>Renouvellement automatique le: ...</p>
+            </div>
+            <button
+              type='button'
+              className={cn("disabled:opacity-40 w-full md:w-1/4 rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary", data?.pricing_plan.title === "essentiel" ? "bg-secondary" : "bg-primary")}
+            >
+              Annuler l'aboonnement
+            </button>
+            <p>Votre abonnement prendra fin le ... à ... (si annulation demandée)</p>
+          </>
+        ) : (
+          <div>
+            <p className='mb-2 text-lg'>Vous n'avez pas d'abonnement pour l'instant.</p>
+            <p className='mb-4'>Choisissez-en un:</p>
+            {plans?.data.map((plan: PlanType) => {
+              const { id, attributes: { title, description, price, access, wedoforyou, youmanage } } = plan
+              return (
+                <div className={cn("flex items-center w-1/2 justify-between border-2 p-4 rounded-md mb-2 hover:p-6 transition-all cursor-pointer", title === "essentiel" ? "border-secondary" : "border-primary")}>
+                  <div>
+                    <p className={cn('uppercase font-bold', title === "essentiel" ? "text-secondary" : "text-primary")}> {title} </p>
+                    <p> {description} </p>
+                  </div>
+                  <p className='text-2xl'>{price} € / mois</p>
+                </div>
+              )
+            })}
+          </div>
+        )
+      }
     </div>
   )
 }
