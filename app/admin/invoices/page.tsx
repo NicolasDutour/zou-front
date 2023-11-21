cc95fc7a0bff5952713987e421ceb1addeaffe3e
nicolas dutour <dutourn99@gmail.com>
2023-11-21 13:05:31 +0100

    feat: add fake invoice

--- app/admin/invoices/page.tsx
+++ app/admin/invoices/page.tsx
@@ -35,8 +35,17 @@ export default async function SettingsInvoicesPage() {
       startDate: "01-10-2023",
       endDate: "30-10-2023",
       plan: "essentiel",
-      amount: 12,
+      amount: 9.99,
       status: "En cours"
+    },
+    {
+      id: 2,
+      invoice_name: formatInvoiceName("premium", "01-10-2023", "30-10-2023"),
+      startDate: "01-09-2023",
+      endDate: "30-09-2023",
+      plan: "premium",
+      amount: 29.99,
+      status: "Payé"
     }
   ]
 
