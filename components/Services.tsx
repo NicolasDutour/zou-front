4aa35399a746855e5639891437e66ad97b3c1e1d
nicolas dutour <dutourn99@gmail.com>
2023-11-26 15:43:36 +0100

    feat: add section for home page

---
+++ components/Services.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Image from "next/image"
+import Link from "next/link";
+
+const imageStyle: React.CSSProperties = {
+  objectFit: "cover",
+};
+
+export default function Services() {
+  return (
+    <section className="bg-white py-20 px-6">
+      <div className="max-w-5xl mx-auto">
+        <h1 className="text-3xl mb-6 font-bold">Votre page ressemblerait à ça</h1>
+        <p className="mr-4">
+          Cliquez <Link className="text-primary font-bold underline underline-offset-4 tracking-widest" href={`${process.env.NEXT_PUBLIC_FRONT_URL}/restaurant/case-a-sausse`} target="_blank">
+            ici
+          </Link>
+          <span className="ml-1">pour avoir un réel aperçu</span>
+        </p>
+        <div className="grid place-items-center mt-4">
+          <Image
+            src={'/design_ready.png'}
+            alt={"design_ready"}
+            style={imageStyle}
+            width={800}
+            height={400}
+            aspect-auto="true"
+            className="rounded-lg"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
