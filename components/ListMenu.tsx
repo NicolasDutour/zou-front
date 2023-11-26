ee1ca2e5fed79aa0b1e98e58922eb1f8882a2656
nicolas dutour <dutourn99@gmail.com>
2023-11-26 15:40:54 +0100

    feat: add criterias end change UI

--- components/ListMenu.tsx
+++ components/ListMenu.tsx
@@ -1,23 +1,23 @@
 "use client"
 
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import Image from "next/image"
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 import Product from './Product';
 
-import { formatIngredients } from '@/lib/utils';
+import { capitalize, formatIngredients } from '@/lib/utils';
 import { ProductType } from "@/lib/types";
 
 const imageStyle: React.CSSProperties = {
@@ -27,18 +27,19 @@ const imageStyle: React.CSSProperties = {
 const ListMenu = ({ products }: {
   products: ProductType[]
 }) => {
-  const [basePizza, setBasePizza] = useState('toutes')
-
-  const handleBase = (e: SetStateAction<string>) => {
-    setBasePizza(e)
-  }
+  const [criteria, setCriteria] = useState("tomate")
+  const criterias = ["tomate", "crème", "végétarien", "dessert"]
 
   const productsFilterByBase = () => {
     if (products?.length > 0) {
-      if (basePizza === 'toutes') {
-        return products
+      if (criteria === 'tomate' || criteria === 'crème') {
+        return products?.filter(product => product.attributes.base === criteria)
+      } else if (criteria === 'végétarien') {
+        return products?.filter(product => product.attributes.vegetarian)
+      } else if (criteria === 'dessert') {
+        return products?.filter(product => product.attributes.dessert)
       }
-      return products?.filter(product => product.attributes.base === basePizza)
+      return products
     }
   }
 
@@ -47,64 +48,60 @@ const ListMenu = ({ products }: {
   }
 
   return (
-    <div className="max-w-5xl mx-auto my-8 px-6">
-      <div className="mb-6">
-        <Select onValueChange={handleBase}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Toutes" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem className="cursor-pointer" value="toutes">Toutes</SelectItem>
-            <SelectItem className="cursor-pointer" value="tomate">Tomate</SelectItem>
-            <SelectItem className="cursor-pointer" value="crème">Crème</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
+    <section className="bg-slate-100 py-8 px-6">
+      <div className="max-w-5xl mx-auto">
+        <Tabs defaultValue="tomate" className="w-full">
+          <TabsList className={`bg-gray-800 mb-6 grid w-full grid-cols-${criterias.length}`}>
+            {criterias.map((criteria, index) => <TabsTrigger className="text-lg hover:bg-white hover:text-gray-800" key={index} onClick={() => setCriteria(criteria)} value={criteria}> {capitalize(criteria)} </TabsTrigger>)}
+          </TabsList>
+          <TabsContent value={criteria}>
+            <div className="grid grid-cols-1 gap-4">
+              {
+                productsFilterByBase()?.map(product => {
+                  const { id, attributes: { product_name, ingredients, price, photo } } = product
+                  let picture;
+                  if (photo?.data?.attributes?.formats) {
+                    if (photo?.data?.attributes?.formats?.small) {
+                      picture = photo?.data?.attributes?.formats?.small.url;
+                    } else if (photo?.data?.attributes?.formats?.medium) {
+                      picture = photo?.data?.attributes?.formats?.medium.url;
+                    } else if (photo?.data?.attributes?.formats?.large) {
+                      picture = photo?.data?.attributes?.formats?.large.url;
+                    } else if (photo?.data?.attributes?.formats?.thumbnail) {
+                      picture = photo?.data?.attributes?.formats?.thumbnail.url;
+                    }
+                  }
 
-      <div className="grid grid-cols-1 gap-4">
-        {
-          productsFilterByBase()?.map(product => {
-            const { id, attributes: { product_name, ingredients, price, photo } } = product
-            let picture;
-            if (photo?.data?.attributes?.formats) {
-              if (photo?.data?.attributes?.formats?.small) {
-                picture = photo?.data?.attributes?.formats?.small.url;
-              } else if (photo?.data?.attributes?.formats?.medium) {
-                picture = photo?.data?.attributes?.formats?.medium.url;
-              } else if (photo?.data?.attributes?.formats?.large) {
-                picture = photo?.data?.attributes?.formats?.large.url;
-              } else if (photo?.data?.attributes?.formats?.thumbnail) {
-                picture = photo?.data?.attributes?.formats?.thumbnail.url;
+                  return (
+                    <Popover key={id}>
+                      <PopoverTrigger>
+                        <Product key={id} product={product} />
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full md:w-96">
+                        <div className="w-full">
+                          <Image
+                            src={picture ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${picture}` : '/no_image.png'}
+                            alt={product_name}
+                            style={imageStyle}
+                            width={400}
+                            height={200}
+                            aspect-auto="true"
+                            className="rounded-lg"
+                          />
+                          <p className="text-gray-900 font-bold py-2 text-2xl">{product_name}</p>
+                          <p className="text-sm mb-4">{formatIngredients(ingredients)}</p>
+                          <p className="text-xl font-bold">{price.toFixed(2) || null} €</p>
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                  )
+                })
               }
-            }
-
-            return (
-              <HoverCard key={id}>
-                <HoverCardTrigger>
-                  <Product key={id} product={product} />
-                </HoverCardTrigger>
-                <HoverCardContent>
-                  <div className="w-full">
-                    <Image
-                      src={picture ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${photo}` : '/no_image.png'}
-                      alt={product_name}
-                      style={imageStyle}
-                      width={400}
-                      height={200}
-                      aspect-auto="true"
-                      className="rounded-lg"
-                    />
-                    <p className="text-gray-900 font-bold py-2">{product_name}</p>
-                    <p className="text-sm mb-4">{formatIngredients(ingredients)}</p>
-                    <p className="text-sm">{price.toFixed(2) || null} €</p>
-                  </div>
-                </HoverCardContent>
-              </HoverCard>
-            )
-          })
-        }
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
-    </div>
+    </section>
   )
 }
 
