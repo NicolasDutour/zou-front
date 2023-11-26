a80d030829ec0f81ce827e90011ecaf15c10be8c
nicolas dutour <dutourn99@gmail.com>
2023-11-26 15:36:03 +0100

    feat: show username in sidebar

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,13 @@
-"use client"
-
-import { useSelector } from "react-redux"
-
-import { SidebarNav } from "./components/SidebarNav"
 
 import { BsStar } from "react-icons/bs"
 import { GoPerson } from "react-icons/go"
 import { IoRestaurantOutline } from "react-icons/io5"
 import { MdOutlineEmojiFoodBeverage } from "react-icons/md"
 import { LiaFileInvoiceSolid } from "react-icons/lia"
+
+import { SidebarNav } from "./components/SidebarNav"
+import { cookies } from "next/headers"
+import { capitalize } from "@/lib/utils"
 
 const sidebarNavItems = [
   {
@@ -42,13 +41,40 @@ interface SettingsLayoutProps {
   children: React.ReactNode
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
-  const user = useSelector((state: any) => state.auth.user)
+async function getData() {
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')?.value
+
+  if (token) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/users/me?populate[restaurants][populate]=*`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
+    if (!res.ok) {
+      console.log("error");
+    }
+    return res.json()
+  }
+}
+
+export default async function SettingsLayout({ children }: SettingsLayoutProps) {
+  const data = await getData()
 
   return (
     <div className="space-y-6 p-10 pb-16 md:block">
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="-mx-4 lg:w-1/5">
+        <aside className="mx-4 lg:w-1/5">
+          {
+            data?.username ? (
+              <div className="shadow-2xl p-4 rounded-2xl mb-4 grid place-items-center">
+                <p className=" text-lg">Bonjour {capitalize(data?.username)}</p>
+              </div>
+            ) : null
+          }
           <SidebarNav items={sidebarNavItems} />
         </aside>
         <div className="flex-1 shadow-custom p-6 rounded-2xl">{children}</div>
