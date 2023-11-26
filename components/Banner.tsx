84c791fb4e3ecbb817562a20e08a147c7e5c1925
nicolas dutour <dutourn99@gmail.com>
2023-11-26 15:37:54 +0100

    feat: get info from db

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,22 @@
-export default function Banner() {
+import { HomeInfoType } from "@/lib/types";
+
+export default function Banner({ homeInfo }: { homeInfo: HomeInfoType }) {
+  const { attributes: { title, subtitle, slogan, slogan_bis, slogan_ter } } = homeInfo
+
   return (
-    <section className="h-[600px] md:h-[500px] w-full bg-green-50">
-      <div className="max-w-5xl mx-auto p-6 h-full flex flex-col justify-center items-center">
-        <h1 className="text-8xl text-secondary mb-6">Ted</h1>
-        <p className="text-4xl mb-4 text-black">Votre page web prêt à l'emploi</p>
-        <p className="text-xl text-gray-500">Un design simple et fonctionnel</p>
-        <p className="text-xl text-gray-500">Vous n'avez qu'à saisir les informations sur votre établissement</p>
-        <p className="text-xl text-gray-500">Et hop votre page est en ligne !</p>
+    <section className="w-full">
+      <div className="relative p-20 bg-cover bg-center h-full bg-hero-pattern flex justify-center items-center">
+        <div className="bg-black opacity-70 absolute inset-0"></div>
+        <div className="mb-10 z-10 text-center tracking-wider">
+          <h1 className="text-8xl font-bold text-primary mb-6 italic"> {title} </h1>
+          <p className="text-4xl mb-4 font-bold text-white"> {subtitle} </p>
+          <p className="text-xl text-white"> {slogan} </p>
+          <p className="text-xl text-white"> {slogan_bis} </p>
+          <p className="text-xl text-white">Et <span className="italic font-bold underline underline-offset-4 tracking-widest text-2xl"> {title}</span> {slogan_ter} </p>
+        </div>
       </div>
     </section>
+
+
   )
 }
