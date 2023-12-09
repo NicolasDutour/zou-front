df6a103e76a5edb6378cd68add1c569a7f4310c0
nicolas dutour <dutourn99@gmail.com>
2023-12-09 22:17:34 +0100

    feat: typing

--- app/admin/components/SidebarNav.tsx
+++ app/admin/components/SidebarNav.tsx
@@ -10,7 +10,7 @@ interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
   items: {
     href: string
     title: string,
-    icon: object
+    icon: React.ReactNode
   }[]
 }
 
@@ -25,21 +25,23 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
       )}
       {...props}
     >
-      {items.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            pathname === item.href
-              ? "bg-muted hover:bg-muted hover:text-primary text-primary font-semibold"
-              : "hover:bg-muted text-gray-400 hover:text-primary",
-            "justify-start text-md p-6 md:last:rounded-b-2xl rounded-none first:rounded-t-2xl tracking-wider"
-          )}
-        >
-          <span className="mr-4 text-2xl"> {item.icon} </span>  {item.title}
-        </Link>
-      ))}
+      {items.map((item) => {
+        return (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              pathname === item.href
+                ? "bg-muted hover:bg-muted hover:text-primary text-primary font-semibold"
+                : "hover:bg-muted text-gray-400 hover:text-primary",
+              "justify-start text-md p-6 md:last:rounded-b-2xl rounded-none first:rounded-t-2xl tracking-wider"
+            )}
+          >
+            <span className="mr-4 text-2xl"> {item.icon} </span>  {item.title}
+          </Link>
+        )
+      })}
     </nav>
   )
 }
