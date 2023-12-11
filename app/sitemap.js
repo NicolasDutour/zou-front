b4a700d3a16fcb48774b1d400d00d65ab50ce1ba
nicolas dutour <dutourn99@gmail.com>
2023-12-11 17:18:23 +0100

    fix: try sitemap with no prerendering

---
+++ app/sitemap.js
@@ -0,0 +1,42 @@
+async function getAllRestaurants() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/restaurants`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+  if (!response.ok) {
+    console.log("error");
+  }
+
+  return response.json();
+}
+
+const NoSSRYourComponent = async () => {
+  const restaurantsData = await getAllRestaurants()
+
+  const restaurants = restaurantsData?.data?.map(({ attributes: { slug, updatedAt } }) => ({
+    url: `${process.env.NEXT_PUBLIC_FRONT_URL}/restaurant/${slug}`,
+    lastModified: updatedAt,
+    changeFrequency: 'yearly',
+    priority: 1,
+  }))
+
+  const routes = [''].map((route) => ({
+    url: `${process.env.NEXT_PUBLIC_FRONT_URL}/${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'yearly',
+    priority: 0.8,
+  }))
+
+  return [...restaurants, ...routes]
+}
+
+// export it with SSR disabled
+const YourComponent = dynamic(() => Promise.resolve(NoSSRYourComponent), {
+  ssr: false,
+})
+
+export default YourComponent
