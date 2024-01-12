74c9a3f17fc2e2dc93839ab25d3913a20f19776b
nicolas dutour <dutourn99@gmail.com>
2024-01-12 10:38:53 +0100

    fix: get image from cloudinary if prod

--- components/MenuFile.tsx
+++ components/MenuFile.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link'
 import { FileType } from "@/lib/types"
 
 export default function MenuFile({ file }: { file: FileType }) {
+  const environment = process.env.NODE_ENV
+
   return (
-    <Link href={`${process.env.NEXT_PUBLIC_STRAPI_URL}${file.attributes.url}`} target='_blank' className="p-6 rounded-md border border-primary bg-white hover:bg-primary hover:text-white transition-all flex justify-center items-center cursor-pointer">Téléchargez <span className='text-primary font-semibold underline underline-offset-4'> {file.attributes.name}</span> </Link>
+    <Link href={environment === 'production' ? file.attributes.url : `${process.env.NEXT_PUBLIC_STRAPI_URL}${file.attributes.url}`} target='_blank' className="p-6 rounded-md border border-primary bg-white hover:bg-primary hover:text-white transition-all flex justify-center items-center cursor-pointer">Téléchargez le menu <span className='underline underline-offset-4 ml-4'> {file.attributes.name}</span> </Link>
   )
 }
