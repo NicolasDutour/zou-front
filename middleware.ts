a207fcd523334be68d3a3650957c7bffdcaf37b4
nicolas dutour <dutourn99@gmail.com>
2024-01-15 17:18:22 +0100

    feat: add protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,43 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
-export function middleware(request: Request) {
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const token = request.cookies.get('token')
+  const isAuthRoute = path === '/login' || path === '/register'
+  const isPrivateRoute = path.startsWith('/admin')
+
+
+  if (isAuthRoute && token?.value) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (isPrivateRoute && !token?.value) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
+  }
 
   // Store current request url in a custom header, which you can read later
-  const requestHeaders = new Headers(request.headers);
-  // requestHeaders.set('x-url', request.url);
-  requestHeaders.set('referer', request.url);
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('referer', request.url)
 
   return NextResponse.next({
     request: {
       // Apply new request headers
       headers: requestHeaders,
     }
-  });
+  })
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/admin/:path*',
+    '/login',
+    '/register',
+    '/faq',
+    '/legal-notice',
+    '/personal-data',
+    '/forgot-password',
+    '/restaurant/:path*'
+  ],
 }
