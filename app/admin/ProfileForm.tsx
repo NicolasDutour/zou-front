9680307d64d4962e3fed98fd9726f5568fe4e7d3
nicolas dutour <dutourn99@gmail.com>
2024-01-16 11:48:27 +0100

    feat: clear unused code

--- app/admin/ProfileForm.tsx
+++ app/admin/ProfileForm.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
