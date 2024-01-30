77c8d8b12bd69a613f97dbf9ae3ad34f8ab67bb6
nicolas dutour <dutourn99@gmail.com>
2024-01-30 20:09:23 +0100

    feat: create no invoice component

---
+++ components/pages/admin/invoice/NoInvoice.tsx
@@ -0,0 +1,9 @@
+export const NoInvoice = () => {
+  return (
+    <div className="mt-4 w-full rounded-2xl bg-muted p-4 md:w-1/2">
+      <div className='space-y-2'>
+        <p>Vous n'avez pas encore de factures</p>
+      </div>
+    </div>
+  )
+}
