af9b5e748c9602f06adbcff50828b4c8da449678
nicolas dutour <dutourn99@gmail.com>
2024-01-31 13:57:50 +0100

    fix: close menu only on mobile

--- components/pages/admin/SidebarNavMobile.tsx
+++ components/pages/admin/SidebarNavMobile.tsx
@@ -7,7 +7,7 @@ export function SidebarNavMobile({ items }: { items: SidebarNavItemType[] }) {
     <nav className="fixed right-0 top-[60px] flex size-full flex-col overflow-auto bg-white lg:hidden">
       {items.map((item, index) => {
         return (
-          <SidebarNavItem key={index} item={item} />
+          <SidebarNavItem key={index} item={item} mobile={true} />
         )
       })}
       <LogoutButton />
