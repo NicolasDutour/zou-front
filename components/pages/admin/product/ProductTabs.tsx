f7dd20c557fc3ddfe1152b1bcf912cc8c7eede7b
nicolas dutour <dutourn99@gmail.com>
2024-02-06 16:13:33 +0100

    feat: show list only for now

--- components/pages/admin/product/ProductTabs.tsx
+++ components/pages/admin/product/ProductTabs.tsx
@@ -9,10 +9,10 @@ export const ProductTabs = ({ products }: { products: ProductType[] }) => {
 
   return (
     <Tabs defaultValue="list" className="w-full">
-      <TabsList className="mb-6 grid w-full grid-cols-2 gap-1 bg-gray-600 md:w-1/2 md:grid-cols-2 lg:w-1/4">
+      {/* <TabsList className="mb-6 grid w-full grid-cols-2 gap-1 bg-gray-600 md:w-1/2 md:grid-cols-2 lg:w-1/4">
         <TabsTrigger className="hover:bg-white hover:text-gray-800" value="list">Liste</TabsTrigger>
         <TabsTrigger className="hover:bg-white hover:text-gray-800" value="visual">Visuel</TabsTrigger>
-      </TabsList>
+      </TabsList> */}
       <TabsContent value="list">
         <ProductsList products={products} />
       </TabsContent>
