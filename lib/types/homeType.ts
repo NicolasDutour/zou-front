6727a98d43af09ceaeda57f067409d6983317ed2
nicolas dutour <dutourn99@gmail.com>
2024-02-13 13:31:20 +0100

    feat: change value

--- lib/types/homeType.ts
+++ lib/types/homeType.ts
@@ -9,7 +9,7 @@ export type HomeInfoType = {
     slogan_bis: string
     slogan_ter: string
     description: string
-    price: number
+    time: number
     home_banner_photo: {
       data: {
         attributes: {
@@ -37,56 +37,34 @@ export type HomeInfoType = {
   }
 }
 
-export type PlanType = {
-  id: number,
-  attributes: {
-    title: string
-    description: string
-    price: number
-    access?: string
-    wedoforyou?: string,
-    youmanage?: string
-  }
-}
-
-export type StepType = {
-  title: string
-  description?: string
-  time?: number
-  icon?: React.ReactElement
-  price?: number
-  option?: boolean
-  actionButton?: string
-}
-
-export type ServiceType = {
-  id: number
-  attributes: {
-    title: string
-    description: string
-    photo: {
-      data: {
-        attributes: {
-          formats: {
-            thumbnail: {
-              name: string
-              url: string
-            }
-            small: {
-              name: string
-              url: string
-            }
-            medium: {
-              name: string
-              url: string
-            }
-            large: {
-              name: string
-              url: string
-            }
-          }
-        }
-      }
-    }
-  }
-}
+// export type ServiceType = {
+//   id: number
+//   attributes: {
+//     title: string
+//     description: string
+//     photo: {
+//       data: {
+//         attributes: {
+//           formats: {
+//             thumbnail: {
+//               name: string
+//               url: string
+//             }
+//             small: {
+//               name: string
+//               url: string
+//             }
+//             medium: {
+//               name: string
+//               url: string
+//             }
+//             large: {
+//               name: string
+//               url: string
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
