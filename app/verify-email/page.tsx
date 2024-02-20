d4eb84f773ee2e248fb90eb7c20eb120f85f713d
nicolas dutour <dutourn99@gmail.com>
2024-02-20 15:22:30 +0100

    feat: addd verify email page

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,10 +1,14 @@
+import { cookies } from "next/headers"
 import Image from "next/image"
 
 export default function VerifyEmailPage() {
+  const cookieStore = cookies()
+  const email = cookieStore.get('email')?.value
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="mx-auto max-w-3xl text-center">
-        <div className="relative h-64 w-64 mb-6">
+        <div className="relative mb-6 size-64">
           <Image
             src="/email-sent.png"
             alt="email-sent"
@@ -14,8 +18,8 @@ export default function VerifyEmailPage() {
           />
         </div>
         <h1 className="text-2xl font-medium">Check your email</h1>
-        <p className="text-gray-400 mt-6">We've sent a verification link to</p>
-        <p className="text-gray-600">email link</p>
+        <p className="mt-6 text-gray-400">We've sent a verification link to</p>
+        <p className="text-gray-600"> {email} </p>
       </div>
     </div>
   )
